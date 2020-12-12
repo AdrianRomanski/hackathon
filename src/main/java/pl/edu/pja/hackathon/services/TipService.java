a39fd21b24adee46cf6b5f89b18ffb6ba78b4d37
Lotnest <lotnestyt@gmail.com>
2020-12-12 13:10:54 +0100

    Added TipController, TipRepositories and TipServices

---
+++ src/main/java/pl/edu/pja/hackathon/services/TipService.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.hackathon.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import pl.edu.pja.hackathon.model.Category;
+import pl.edu.pja.hackathon.model.tip.Tip;
+
+public interface TipService {
+
+    Page<Tip> getAllTipsPaged(Pageable pageable);
+
+    Tip getCategoryById(Long id);
+}
