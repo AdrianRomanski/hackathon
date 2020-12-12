8d143fbe246bf4e001cccc6f45020c04741ffe0e
adrianromanski <adrianromanski@icloud.com>
2020-12-12 13:50:08 +0100

    Final Grid Related Bug Fixes

--- src/main/java/pl/pja/edu/hackathon/controllers/HomeController.java
+++ src/main/java/pl/pja/edu/hackathon/controllers/HomeController.java
@@ -2,7 +2,6 @@ package pl.pja.edu.hackathon.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
